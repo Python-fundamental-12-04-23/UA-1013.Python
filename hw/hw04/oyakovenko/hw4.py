ddb0fb24b64f2a4035e35c02224eeebea6ad60cb
oyakovenko <alex231@ukr.net>
2023-04-21 22:39:43 +0300

    Third Home Work_fixed

--- hw/hw04/oyakovenko/hw4.py
+++ hw/hw04/oyakovenko/hw4.py
@@ -1,8 +1,8 @@
 val = int(input('Please, enter the temperature in Celsius: '))
 
 print(f'Enter the temperature in Celsius: {val}')
-far_t = round((val*9/5)+32)
 if val < -273.15:
     print('Error: Temperature below absolute zero (-273.15°C)')
 else:
+    far_t = round((val * 9 / 5) + 32)
     print(f'{val}°C is equivalent to {far_t}°F')
