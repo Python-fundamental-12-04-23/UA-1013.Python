f2960dacd5e63da9d37f3c3015eb63366b63fc94
Sergiy Kiyan <breakworks1@gmail.com>
2023-04-23 12:36:12 +0300

    # Task2

--- hw/hw03/SergiyKiyan/practical_tasks_github.py
+++ hw/hw03/SergiyKiyan/practical_tasks_github.py
@@ -7,36 +7,46 @@
 # - replace all occurrences of the symbol “i” with “&”
 
 
-s="Beautiful is better than ugly. \
-    ▪ Explicit is better than implicit. \
-    ▪ Simple is better than complex. \
-    ▪ Complex is better than complicated.\
-    ▪ Flat is better than nested.\
-    ▪ Sparse is better than dense.\
-    ▪ Readability counts.\
-    ▪ Special cases aren't special enough to break the \
-    rules.\
-    ▪ Although practicality beats purity.\
-    ▪ Errors should never pass silently.\
-    ▪ Unless explicitly silenced.\
-    ▪ In the face of ambiguity, refuse the temptation to \
-    ▪ guess.\
-    ▪ There should be one-- and preferably only one --\
-    obvious way to do it.\
-    ▪ Although that way may not be obvious at first \
-    unless you're Dutch.\
-    ▪ Now is better than never.\
-    ▪ Although never is often better than *right* now.\
-    ▪ If the implementation is hard to explain, it's a bad \
-    idea.\
-    ▪ If the implementation is easy to explain, it may be \
-    a good idea.\
-    ▪ Namespaces are one honking great idea -- let's do \
-    more of those\
-"
-print(" occurrences of the words- better ", s.find("better"), "\n", "occurrences of the words- never", s.find("never"), "\n", "occurrences of the words- is", s.find("is"))
-s=s.upper()
-s=s.replace("I", "&")
-print(s)
+# s="Beautiful is better than ugly. \
+#     ▪ Explicit is better than implicit. \
+#     ▪ Simple is better than complex. \
+#     ▪ Complex is better than complicated.\
+#     ▪ Flat is better than nested.\
+#     ▪ Sparse is better than dense.\
+#     ▪ Readability counts.\
+#     ▪ Special cases aren't special enough to break the \
+#     rules.\
+#     ▪ Although practicality beats purity.\
+#     ▪ Errors should never pass silently.\
+#     ▪ Unless explicitly silenced.\
+#     ▪ In the face of ambiguity, refuse the temptation to \
+#     ▪ guess.\
+#     ▪ There should be one-- and preferably only one --\
+#     obvious way to do it.\
+#     ▪ Although that way may not be obvious at first \
+#     unless you're Dutch.\
+#     ▪ Now is better than never.\
+#     ▪ Although never is often better than *right* now.\
+#     ▪ If the implementation is hard to explain, it's a bad \
+#     idea.\
+#     ▪ If the implementation is easy to explain, it may be \
+#     a good idea.\
+#     ▪ Namespaces are one honking great idea -- let's do \
+#     more of those\
+# "
+# print(" occurrences of the words- better ", s.find("better"), "\n", "occurrences of the words- never", s.find("never"), "\n", "occurrences of the words- is", s.find("is"))
+# s=s.upper()
+# s=s.replace("I", "&")
+# print(s)
 
-# Task1
+# Task2
+# A four-digit natural number is specified:
+# - find the product of the digits of this number
+# - write the number in reverse order
+# - in ascending order, you need to sort the numbers included in the given number
+
+s=input("Input 4 digit number: ")
+
+product=[int(x) for x in str(s)]
+multy=product[0]*product[1]*product[2]*product[3]
+print("product of the digits - ",multy)
