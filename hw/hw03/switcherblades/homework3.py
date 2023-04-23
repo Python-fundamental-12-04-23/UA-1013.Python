9e65d853a4c33e63e07b635d1384f43976ebeb87
omparkhomchuk <omparkhomchuk@gmail.com>
2023-04-23 14:07:46 +0300

    1task

---
+++ hw/hw03/switcherblades/homework3.py
@@ -0,0 +1,10 @@
+# import shutil
+# txt_path = r'E:\workspace\pycharm_projects\pythonFundamental\UA-1013.Python\hw\hw01\switcherblades\Zen.txt'
+# dst_path = r'E:\workspace\pycharm_projects\pythonFundamental\UA-1013.Python\hw\hw03\switcherblades'
+# shutil.copy(txt_path, dst_path)
+with open("Zen.txt") as file:
+    text = file.read()
+print("Count of 'better' =", text.count("better"))
+print("Count of 'never' =", text.count("never"))
+print("Count of 'is' =", text.count("is"))
+#-------------------------------------------
