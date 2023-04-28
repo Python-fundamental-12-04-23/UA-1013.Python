37ae15e4036d6e43bc0db4b6aac0dbddb05ebb86
omparkhomchuk <omparkhomchuk@gmail.com>
2023-04-28 12:24:04 +0300

    made a string

--- hw/hw03/switcherblades/homework3.py
+++ hw/hw03/switcherblades/homework3.py
@@ -1,9 +1,23 @@
-import shutil
-txt_path = r'E:\workspace\pycharm_projects\pythonFundamental\UA-1013.Python\hw\hw01\switcherblades\Zen.txt'
-dst_path = r'E:\workspace\pycharm_projects\pythonFundamental\UA-1013.Python\hw\hw03\switcherblades'
-shutil.copy(txt_path, dst_path)
-with open("Zen.txt") as file:
-    text = file.read()
+text = """Beautiful is better than ugly.
+        Explicit is better than implicit.
+        Simple is better than complex.
+        Complex is better than complicated.
+        Flat is better than nested.
+        Sparse is better than dense.
+        Readability counts.
+        Special cases aren't special enough to break the rules.
+        Although practicality beats purity.
+        Errors should never pass silently.
+        Unless explicitly silenced.
+        In the face of ambiguity, refuse the temptation to guess.
+        There should be one-- and preferably only one --obvious way to do it.
+        Although that way may not be obvious at first unless you're Dutch.
+        Now is better than never.
+        Although never is often better than right now.
+        If the implementation is hard to explain, it's a bad idea.
+        If the implementation is easy to explain, it may be a good idea.
+        Namespaces are one honking great idea – let's do more of those!
+        """
 print("Count of 'better' =", text.count("better"))
 print("Count of 'never' =", text.count("never"))
 print("Count of 'is' =", text.count("is"))
