70a42d11cfde71155aa324edd391be3bf1dad76f
Aksyonov Oleg <software.vhs@gmail.com>
2023-04-28 22:32:34 +0300

    added_files

---
+++ hw/hw03/oyakovenko/hw3_3.py
@@ -0,0 +1,11 @@
+#interchange the values of two variables with out using the third variable
+
+var_1 = 10
+var_2 = 15
+
+var_1 = var_1 + var_2
+var_2 = var_1 - var_2
+var_1 = var_1-var_2
+
+print(var_1)
+print(var_2)
