14682ab3c900bc5a371fe7f475b921fa47fbc22a
Sergiy Kiyan <breakworks1@gmail.com>
2023-05-01 09:20:24 +0300

    # Homework\Task 3

--- hw/hw05/SergiyKiyan/task1_task2.py
+++ hw/hw05/SergiyKiyan/task1_task2.py
@@ -2,25 +2,26 @@
 # use the loop to change the type of these elements to a floating
 # type.
 # (Hint: use the built-in float () function)
-elements_of_int=list("3432234324")
+elements_of_int=['3','4','3','str','2','3','4','3','not_int','4']
 print(elements_of_int)
 i=0
 for x in elements_of_int:
-    elements_of_int[i]=float(elements_of_int[i])
- #   elements_of_int[i]=x
- #   print(x,elements_of_int[i],i)
+    try:
+        elements_of_int[i]=float(elements_of_int[i])
+    except:
+        elements_of_int[i] = elements_of_int[i]
     i+=1
 print(elements_of_int)
+
+
 
 # Task2. Print Fibonacci numbers up to the entered number n,
 # using cycles.
 # (Sequence of Fibonacci numbers 0, 1, 1, 2, 3, 5, 8, 13, etc.)
 
 fibonacci_result=input("Enter last number in a fibonacci row: ")
-try:
-    fibonacci_result=int(fibonacci_result)
-except:
-    print("Please enter a positive integer, starting with 0")
+fibonacci_result=int(fibonacci_result)
+
 n1=0
 n2=1
 count=0
@@ -41,6 +42,8 @@ else:
         n2=combination
         count+=1
 
+
+
 #Task 3
 # write a script to calculate the factorial of entered numbers
 # Ex: 0!=1, 1!=1, 2!=2, 3!=1*2*3=6
@@ -48,16 +51,13 @@ else:
 
 print('\n')
 factorial_result=input("Enter last number for a factorial: ")
-try:
-    factorial_result=int(factorial_result)
-except:
-    print("Please enter a positive integer, starting with 0")
+factorial_result=int(factorial_result)
 n1=1
 count=1
 if factorial_result<0:
     print("Please enter a positive integer, starting with 0")
-# elif factorial_result==1:
-#     print("factorial row, count starts with  :", n1
+elif factorial_result==0:
+    print("Factorial result  :", n1)
 else:
     while n1<=factorial_result:
         count=n1*count
