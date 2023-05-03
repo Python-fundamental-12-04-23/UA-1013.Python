a70cd5792219b15585e7e738b9b9c63879181e08
omparkhomchuk <omparkhomchuk@gmail.com>
2023-05-03 13:38:05 +0300

    homework5

--- hw/hw05/switcherblades/homework5.py
+++ hw/hw05/switcherblades/homework5.py
@@ -2,6 +2,7 @@ int_list = [int(x) for x in range(1, 10)]
 float_list = [float(x) for x in int_list]
 print(float_list)
 #---------------------------------------
+
 n = int(input())
 a = 1
 y = 0
@@ -10,6 +11,7 @@ for i in range(1, n+1):
     a = b + y
     y = b
     print(b, end=' ')
+
 #----------------------------------------
 n = int(input())
 res = 1
