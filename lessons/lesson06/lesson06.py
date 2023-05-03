7a20f8b99768abf2e1e71907bbee35db0ffb0ced
Mykhailo Vivcharenko <mihail.vivcharenko@gmail.com>
2023-05-03 16:51:50 +0300

    .

--- lessons/lesson06/lesson06.py
+++ lessons/lesson06/lesson06.py
@@ -69,25 +69,30 @@ from copy import deepcopy
 # false_en = enumerate("false")
 # print(list(false_en))
 # for i, element in enumerate(false):
-# #     print(i, element)
-# l = [1,5,2,3,7,4,3]
+#     print(i, element)
+# l = [1, 5, 2, 3, 7, 4, 3]
 # print(sorted(l))
 # l = [1,5,"2",3,"7",4,3]
 # l = "0 one 0 one one"
 # temp = []
 # for i in l.split():
-#
+
 #     if "0" == i:
 #         temp.append(0)
-#     elif "one"  == i:
+#     elif "one" == i:
 #         temp.append(1)
 # print(temp)
+
+
 # def mys(element):
 #     return int(element)
-# # print(sorted(l, key=mys))
-# # print(sorted(l, key=lambda x: int(x)))
+
+
+# print(sorted(l, key=mys))
+# print(sorted(l, key=lambda x: int(x)))
 # print([i for i in dir(list) if not i.startswith("_")])
 # print([i for i in dir(tuple) if not i.startswith("_")])
+
 # t = tuple("test")
 # print(t)
 # t = tuple()
@@ -96,9 +101,9 @@ from copy import deepcopy
 # print(t)
 # t = (1,)
 # print(t)
-# print(sorted((1,2,5,2,3,4)))
-# t = (1,2, [1,2])
-# # t[0] = 15
+# print(sorted((1, 2, 5, 2, 3, 4)))
+# t = (1, 2, [1, 2])
+# t[0] = 15
 # t[2][1] = 999
 # print(t)
 # l = [i for i in range(999)]
@@ -115,8 +120,8 @@ from copy import deepcopy
 #
 # s = {"q", 1, 1, "qq", "q", 1}
 # print(s)
-# # s = {}
-# # print(type(s), s)
+# s = {}
+# print(type(s), s)
 # s.add("ss")
 # print(s)
 # print(s.pop())
@@ -144,7 +149,7 @@ from copy import deepcopy
 # print(d.get("key_1"))
 # print(d.get("key_11"))
 # print(d.get("key_11", []))
-# # print(d["key_11"])
+# print(d["key_11"])
 # dd = d.fromkeys(["dsfds", 1, 12], [1, 2, 3])
 # dd["h"] = "dsf"
 # print(dd)
@@ -175,8 +180,8 @@ from copy import deepcopy
 #      if value == [1, 2, 3]:
 #           print(key, value)
 #
-# l = [i**2 for i in range(10)]
-# print(l)
+l = [i**2 for i in range(10)]
+print(l)
 # l = []
 # for i in range(10):
 #      l.append(i**2)
@@ -188,17 +193,17 @@ from copy import deepcopy
 # t = (i for i in range(10))
 # print(t)
 
-l = [i ** 2 for i in range(10)]
-print(l)
-l = [i ** 2 for i in range(10) if i % 2]
-print(l)
-l = [(i, j, k) for i in range(10) for j in range(i)
-     for k in range(i + j) if (i + j + k) % 2]
-print(l)
-l = []
-for i in range(10):
-    for j in range(i):
-        for k in range(i + j):
-            if (i + j + k) % 2:
-                l.append((i, j, k))
-print(l)
+# l = [i ** 2 for i in range(10)]
+# print(l)
+# l = [i ** 2 for i in range(10) if i % 2]
+# print(l)
+# l = [(i, j, k) for i in range(10) for j in range(i)
+#      for k in range(i + j) if (i + j + k) % 2]
+# print(l)
+# l = []
+# for i in range(10):
+#     for j in range(i):
+#         for k in range(i + j):
+#             if (i + j + k) % 2:
+#                 l.append((i, j, k))
+# print(l)
