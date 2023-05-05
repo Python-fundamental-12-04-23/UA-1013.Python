772dd310d1dfd8a047431d7f1c8d7ba15f01cc4b
Parcival <ipavlenko239@gmail.com>
2023-05-05 15:39:35 +0300

    fixed commenting of some code

--- hw/hw07/parcival22/first_task/homework.py
+++ hw/hw07/parcival22/first_task/homework.py
@@ -11,30 +11,31 @@ def largest(*args):
 # -------------------------
 
 # second task
-# def main():
-#     PI = 3.14
+def main():
+    PI = 3.14
 
-#     def circle_area(r):
-#         nonlocal PI
-#         return PI * pow(r, 2)
-#     def rect_area(l, b):
-#         return l * b
-#     def triangle_area(b, h):
-#         return b * h / 2
+    def circle_area(r):
+        nonlocal PI
+        return PI * pow(r, 2)
     
-#     command = input("Hello, user! I can calculate the area of circle, triangle and rectangle!\nWhich figure`s area do you want me to find? \n")
+    def rect_area(l, b):
+        return l * b
+    def triangle_area(b, h):
+        return b * h / 2
+    
+    command = input("Hello, user! I can calculate the area of circle, triangle and rectangle!\nWhich figure`s area do you want me to find? \n")
 
-#     if command == 'triangle' or command == 'rectangle':
-#         a = int(input(f"Nice, now give me the {'base' if command == 'triangle' else 'length'} of the {command}: "))
-#         b = int(input(f"And {'height' if command == 'triangle' else 'width'}, please: "))
-#         area = triangle_area(a, b) if command == 'triangle' else rect_area(a, b)
-#         print(f"The area of your {command} is {area}")
-#     elif command == 'circle':
-#         radius = int(input("Please give me the radius of your circle: "))
-#         area = circle_area(radius)
-#         print(f"The area of your {command} is {area}")
-#     else:
-#         print("Sorry, I cannot calculate this figure")
+    if command == 'triangle' or command == 'rectangle':
+        a = int(input(f"Nice, now give me the {'base' if command == 'triangle' else 'length'} of the {command}: "))
+        b = int(input(f"And {'height' if command == 'triangle' else 'width'}, please: "))
+        area = triangle_area(a, b) if command == 'triangle' else rect_area(a, b)
+        print(f"The area of your {command} is {area}")
+    elif command == 'circle':
+        radius = int(input("Please give me the radius of your circle: "))
+        area = circle_area(radius)
+        print(f"The area of your {command} is {area}")
+    else:
+        print("Sorry, I cannot calculate this figure")
 
 # ---------------
 
