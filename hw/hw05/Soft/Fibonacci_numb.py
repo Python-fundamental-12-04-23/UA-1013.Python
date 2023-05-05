f1ad884891717113c1a28eec9d959fa7b001ad22
Aksyonov Oleg <software.vhs@gmail.com>
2023-05-05 20:56:38 +0300

    added_files

---
+++ hw/hw05/Soft/Fibonacci_numb.py
@@ -0,0 +1,10 @@
+
+def fibonacci_numb(num):
+    fib_prev = 0
+    fib_curr = 1
+
+    print(fib_prev)
+
+    while fib_curr <= n:
+        print(fib_curr)
+        fib_prev, fib_curr = fib_curr, fib_prev + fib_curr
