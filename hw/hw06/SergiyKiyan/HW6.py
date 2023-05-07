629f7918f8a141a337495e836bda654f83aad10a
Sergiy Kiyan <breakworks1@gmail.com>
2023-05-07 15:05:37 +0300

    # Homework6\Task 1

---
+++ hw/hw06/SergiyKiyan/HW6.py
@@ -0,0 +1,15 @@
+#Task 1. In the range from 1 to 10 frtermaine:
+# even number that a divisible by 2
+# odd munber whicj are divisible by 3
+# number that are not divisible by 2 and 3
+
+for x in range(11):
+    if not x%2:
+        print("It is even number divisible by 2:  ",x)
+    elif not x%3:
+        print("It is odd number divisible by 3:  ",x)
+    else:
+        print("number that are not divisible by 2 and 3",x)
+
+
+
