c0dcca45f83ae0399062eca5fab49aec325a7803
Sergiy Kiyan <breakworks1@gmail.com>
2023-05-09 12:57:41 +0300

    # Homework7_2\Task 6,7

--- hw/hw07/SergiyKiyan/HW7_2.py
+++ hw/hw07/SergiyKiyan/HW7_2.py
@@ -1,4 +1,4 @@
-#first task
+#task 1
 def greet(name):
     if name == "Johnny":
         return "Hello, my love!"
@@ -25,3 +25,23 @@ def reverse(st):
     new = " ".join(st)
 
     return new
+
+#Task 6
+#In this kata you will create a function that takes in a list and returns a list with the reverse order.
+def reverse_list(l):
+    return l[::-1]
+    'return a list with the reverse order of l'
+
+#Task 6
+# You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+#
+# Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+#
+# Function should return true if it is possible and false if not.
+
+def zero_fuel(distance_to_pump, mpg, fuel_left):
+    dist=mpg*fuel_left
+    if dist >= distance_to_pump:
+        return True
+    else:
+        return False
