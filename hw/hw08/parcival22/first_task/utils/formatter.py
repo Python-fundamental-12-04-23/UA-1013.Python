96f96e3a4eda76fb86423fc4e67fb5ac2d605c05
Parcival <ipavlenko239@gmail.com>
2023-05-09 21:45:41 +0300

    homework

---
+++ hw/hw08/parcival22/first_task/utils/formatter.py
@@ -0,0 +1,13 @@
+
+__all__ = ['format_string']
+
+a = 10
+b = 5
+c = 4
+
+def format_string():
+    pass
+
+def somefunc():
+    print('somefunc')
+
---
+++ hw/hw08/parcival22/third_task/helpers.py
@@ -0,0 +1,11 @@
+import math
+
+
+def rect_area(l, b):
+    return l * b
+
+def triangle_area(b, h):
+    return b * h / 2
+
+def circle_area(r): 
+    return math.pi * pow(r, 2)
