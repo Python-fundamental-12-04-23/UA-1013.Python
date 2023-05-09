331d0b752f9253a400ac580b9344e7e50ec6cf13
Sergiy Kiyan <breakworks1@gmail.com>
2023-05-09 09:13:39 +0300

    # Homework7_2\Task 5

--- hw/hw07/SergiyKiyan/HW7_2.py
+++ hw/hw07/SergiyKiyan/HW7_2.py
@@ -13,3 +13,15 @@ def filter_words(st):
 # task 4
 def number_to_string(num):
     return str(num)
+
+#Task 5
+# You need to write a function that reverses the words in a given string. A word can also fit an empty string. If this is not clear enough, here are some examples:
+#
+# As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+#
+# Example (Input --> Output)
+def reverse(st):
+    st = st.split()[::-1]
+    new = " ".join(st)
+
+    return new
