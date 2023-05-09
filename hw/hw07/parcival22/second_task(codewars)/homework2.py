a7061d13f434b72a494600967fa06abb3fbc57cf
Parcival <ipavlenko239@gmail.com>
2023-05-09 12:59:06 +0300

    fixed some imperfections

--- hw/hw07/parcival22/second_task(codewars)/homework2.py
+++ hw/hw07/parcival22/second_task(codewars)/homework2.py
@@ -1,29 +1,36 @@
 import math
 
+# I
 def greet(name):
     return "Hello, my love!" if name == "Johnny" else f"Hello, {name}!"
-     
+
+# II
 def distance(x1, y1, x2, y2):
     sum = pow(x2 - x1, 2) + pow(y2 - y1, 2)
     return round(math.sqrt(sum), 2)
 
+# III
 def filter_words(st):
     s = st.lower().split()
     j = ' '.join(s).capitalize()
     return j
 
+# IV
 def number_to_string(num):
     return str(num)
 
+# V
 def reverse(st):
     l = st.split()
     r = l.reverse()
     return ' '.join(l)
 
+# VI
 def reverse_list(l):
     r = l[::-1]
     return r
 
+# VII
 def solution(number):
     if number < 0:
         return 0
@@ -33,15 +40,19 @@ def solution(number):
             res += x
     return res
 
+# VIII
 def zero_fuel(distance_to_pump, mpg, fuel_left):
     return mpg * fuel_left >= distance_to_pump 
 
+# IX
 def are_you_playing_banjo(name):
     return f"{name} plays banjo" if name.lower().startswith('r') else f"{name} does not play banjo"
 
+# X
 def bool_to_word(boolean):
     return 'Yes' if boolean else 'No'
 
+# XI
 def count_sheeps(sheeps):
     count = 0
     for sheep in sheeps:
@@ -49,6 +60,7 @@ def count_sheeps(sheeps):
             count += 1
     return count
 
+# XII
 def correct_tail(body, tail):
     return body[-1] == tail 
 
