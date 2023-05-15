949116b0d2a609f64587a9456bdae39617557c0b
Sergiy Kiyan <breakworks1@gmail.com>
2023-05-15 14:52:57 +0300

    # Homework7_2\Task 8_2

--- hw/hw08/SergiyKiyan/HW8_2.py
+++ hw/hw08/SergiyKiyan/HW8_2.py
@@ -1,20 +1,24 @@
 #task 2
 password=input("Enter password to check:")
-password_symbols=list(password)
-print(password_symbols)
+status = True
 if len(password)>16:
     print("Max length of password is 16")
+    status=False
 if len(password)<6:
     print("Min length of password is 6")
+    status = False
 if password.find("@")!=-1:
-    print("@ check passed")
+    pass
 elif password.find("#")!=-1:
-        print("# check passed")
+    pass
 elif password.find("$")==-1:
-            print("Password must contain at least one symbol from - $,#,@")
+    print("Password must contain at least one symbol from - $,#,@")
+    status = False
 if not any(i.isdigit() for i in password):
     print("Password must contain at least one number")
+    status = False
 if not any(i.isupper() for i in password):
     print("Password must contain at least one capital letter")
-print(password.find("#"))
+    status = False
+print("Passwword passing status: ",status)
 
