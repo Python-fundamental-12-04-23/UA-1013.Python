93133fb1d90e472a48e4b3faa2f9dd99a76d721c
Sergiy Kiyan <breakworks1@gmail.com>
2023-05-21 21:20:09 +0300

    # Homework7_2\Task 8_3

---
+++ hw/hw08/SergiyKiyan/calculates_the _areas.py
@@ -0,0 +1,13 @@
+
+#Functions to calculate the areas
+def area_of_rectangle(a,b):
+    """The function that returns the area of a rectangle"""
+    return a*b
+
+def area_of_triangle(a,b):
+    """The function that returns the area of a triangle"""
+    return a*b/2
+
+def area_of_circle(r):
+    """The function that returns the area of a circle """
+    return math.pi*pow(r,2)
