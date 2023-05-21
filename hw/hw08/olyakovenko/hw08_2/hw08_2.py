162a9d9ea0fdeebc3954301613432c20f7c90611
oyakovenko <alex231@ukr.net>
2023-05-21 12:31:30 +0300

    HW08_fixed

--- hw/hw08/olyakovenko/hw08_2/hw08_2.py
+++ hw/hw08/olyakovenko/hw08_2/hw08_2.py
@@ -3,7 +3,7 @@ import re
 
 def check_password():
     psw = input('Please, enter the password: ')
-    pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$#])[A-Za-z\d@$!%*#?&]{8,16}$'
+    pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$#])[A-Za-z\d@$!%*#?&]{6,16}$'
     if re.match(pattern, psw) is None:
         return False
     else:
