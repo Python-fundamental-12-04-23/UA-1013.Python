624fe514543763ff0aa28e01fac663f1ed46e214
Aksyonov Oleg <software.vhs@gmail.com>
2023-05-26 22:55:16 +0300

    added class_human and polygon_class

--- hw/hw10/Soft/Polygon class.py
+++ hw/hw10/Soft/Polygon class.py
@@ -1,2 +1,16 @@
 
 
+class Polygon:
+    def __init__(self, sides):
+        self.sides = sides
+
+
+class Rectangle(Polygon):
+    def __init__(self, width, height):
+        super().__init__(4)
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
