64cf2d4eed4199479e74e1a5770e5627c0a0025a
Sergiy Kiyan <breakworks1@gmail.com>
2023-05-30 22:18:53 +0300

    # Homework9\Task 10_1

---
+++ hw/hw10/SergiyKiyan/HW10_1.py
@@ -0,0 +1,14 @@
+#Task 1
+class Polygon():
+    pass
+class Rectangle(Polygon):
+    def __init__(self,a_side,b_side ):
+        self.a_side=a_side
+        self.b_side=b_side
+
+
+    def square(self):
+        return self.a_side*self.b_side
+
+square_rect=Rectangle(10,5)
+print(square_rect.square())
