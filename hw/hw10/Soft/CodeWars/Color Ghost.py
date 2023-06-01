82cdc6a016313aea7337adefe2c2737369c6fe71
Aksyonov Oleg <software.vhs@gmail.com>
2023-06-01 18:26:07 +0300

    code wars

--- hw/hw10/Soft/CodeWars/Color Ghost.py
+++ hw/hw10/Soft/CodeWars/Color Ghost.py
@@ -1,7 +1,6 @@
+
+
 # Color Ghost
-# Create a class Ghost
-# Ghost objects are instantiated without any arguments.
-# Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
 
 import random
 
