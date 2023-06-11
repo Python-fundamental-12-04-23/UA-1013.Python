b06043c373812ba5df9bcb1fa27910d03a533a6b
Sergiy Kiyan <breakworks1@gmail.com>
2023-06-11 21:33:04 +0300

    # Homework10\Task CodeWar2

--- hw/hw10/SergiyKiyan/CodeWar.py
+++ hw/hw10/SergiyKiyan/CodeWar.py
@@ -79,3 +79,9 @@ class Sphere:
         return round(d, 5)
 
 
+#Python's Dynamic Classes
+def class_name_changer(cls, new_name: str):
+    if new_name[0].isupper and new_name.isalnum():
+        cls.__name__ = new_name
+    else:
+        raise Exception('Invalid class name')
