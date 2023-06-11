12b530ba38590e70ab30d40b7f05900b31c3534c
Sergiy Kiyan <breakworks1@gmail.com>
2023-06-11 19:59:38 +0300

    # Homework10\Task 10_2

--- hw/hw10/SergiyKiyan/HW10_1.py
+++ hw/hw10/SergiyKiyan/HW10_1.py
@@ -14,3 +14,19 @@ square_rect=Rectangle(10,5)
 print(square_rect.square())
 
 #Task 2
+# Create a class Human, everyone has a name, create a method in the
+# class that displays a welcome message to each person. Create a class method
+# in the class that returns information that it is a species of "Homosapiens". And
+# in the class create a static method that returns an arbitrary message.
+
+class Human:
+    def __init__(self,name):
+        def welcome_mesage(self):
+            print(f"Welcome,{self.name}!")
+            @classmethod
+            def species(cls):
+                return "Homosapiens"
+
+            @staticmethod
+            def rbitrary_message():
+                return "an arbitrary message"
