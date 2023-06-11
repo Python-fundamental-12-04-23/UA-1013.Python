79133b3d9eb0b630c33fe590ff4911b069a7dc10
Sergiy Kiyan <breakworks1@gmail.com>
2023-06-11 20:20:07 +0300

    # Homework10\Task 10_3

--- hw/hw10/SergiyKiyan/HW10_1.py
+++ hw/hw10/SergiyKiyan/HW10_1.py
@@ -30,3 +30,41 @@ class Human:
             @staticmethod
             def rbitrary_message():
                 return "an arbitrary message"
+
+
+# Task3.
+# Create an employee class. Each employee has characteristics such as name
+# and salary. The class should have a counter that calculates the total number of
+# employees, as well as a method that prints the total number of employees and a
+# method that displays information about each employee in particular, namely the
+# name and salary.
+# In addition to creating a class, display information about the base classes from which
+# the employee class is inherited (__base__), the class namespace (__dict__), the
+# class name (__name__), the module name in which the class is defined
+# (__module__), the documentation bar ( __doc__)
+
+class Employee:
+    employees_count=0
+
+    def __init__(self, name, salary):
+        self.name=name
+        self.salary=salary
+        Employee.employees_count+=1
+
+    @classmethod
+    def total_number_of_employees(self):
+        print(f"total number of employees: {self.employees_count}")
+
+    def information_about_each_employee(self):
+        print(f"Emploee name: {self.name} \n Emploee salary: {self.salary}")
+
+    def the_base_classes(self):
+        print(Employee.__base__)
+        print(Employee.__dict__)
+        print(Employee.__name__)
+        print(Employee.__module__)
+        print(Employee.__doc__)
+
+
+
+
