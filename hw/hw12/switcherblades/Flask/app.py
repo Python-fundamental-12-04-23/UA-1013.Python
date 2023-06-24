ebbf6d5a6bf6884f546fb69dbdaad5dc59b2fc42
omparkhomchuk <omparkhomchuk@gmail.com>
2023-06-24 11:59:05 +0300

    lesson3 Flask

--- hw/hw12/switcherblades/Flask/app.py
+++ hw/hw12/switcherblades/Flask/app.py
@@ -1,36 +1,15 @@
 from markupsafe import escape
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return f'User {escape(username)}'
-
-
-@app.route("/post/<int:post_id>")
-def show_post(post_id):
-    return f"Post {post_id}"
-
-@app.route("/path/<path:subpath>")
-def show_subpath(subpath):
-    return f"Subpath {escape(subpath)}"
-
-@app.route('/projects/')
-def projects():
-    return 'The project page'
-
-
-@app.route('/about')
-def about():
-    return 'The about page'
-
-
+@app.route('/capitalize/<word>/')
+def capitalize(word):
+    return '<h1>{}</h1>'.format(escape(word.capitalize()))
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
